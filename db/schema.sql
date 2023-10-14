27bd2b77a4adee73ff380645a37458d2b27e98aa
davep80 <davidpaq1@gmail.com>
2023-10-14 14:53:50 -0400

    sql trigger function

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,20 @@ CREATE TABLE client_user (
     CONSTRAINT unique_client_user UNIQUE (first_name, last_name, address)
 );
 
+-- Create table in DATABASE FIRST. on each successful addition of new user to users table, new log is made in auth_users table
+create table auth_users (user_id integer not null unique, user_email text not null unique, is_auth Boolean default false);
+
+CREATE OR REPLACE FUNCTION insert_auth_user()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO auth_users (user_id, user_email, is_auth)
+    VALUES (NEW.id, NEW.email, false);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER users_insert_trigger
+AFTER INSERT ON client_user
+FOR EACH ROW
+EXECUTE FUNCTION insert_auth_user();
+
