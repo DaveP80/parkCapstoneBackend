d5eb4748e17745ed850a4a596dea55d5b3cdce04
davep80 <davidpaq1@gmail.com>
2023-10-28 11:12:26 -0400

    fix user/profile query

--- queries/users.js
+++ queries/users.js
@@ -246,7 +246,7 @@ const getInfo = async (args) => {
       r.renter_email,
       r.background_verified,
       r.r_pmt_verified,
-      au.is_auth
+      au.all_is_auth
     from
       (
       select
