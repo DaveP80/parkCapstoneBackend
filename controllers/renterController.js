b7e98e15ef538189e73dcd281bfc29b9b424a151
davep80 <davidpaq1@gmail.com>
2023-11-05 22:25:56 -0500

    create an update parking spaces

--- controllers/renterController.js
+++ controllers/renterController.js
@@ -1,21 +1,14 @@
 const {
-  createRenter,
   createProperty,
   getPropInfo,
+  spaceAndPropInfo,
+  createSpaces,
+  updateSpaces,
+  createRenter,
   updateRenterAddress,
 } = require("../queries/renters");
 
 const { parsedMessage, stc } = require("../lib/helper/helper");
-
-const createRenterFunc = async (req, res, next) => {
-  createRenter(req.body)
-    .then((response) => {
-      res.json(response);
-    })
-    .catch((e) => {
-      res.status(stc(e)).json({ message: e.message, error: e.error });
-    });
-};
 
 const createNewProperty = async (req, res, next) => {
   createProperty(req.body)
@@ -26,13 +19,52 @@ const createNewProperty = async (req, res, next) => {
       res.status(stc(e)).json({ message: e.message, error: e.error });
     });
 };
-//Endpoint for authenticating user after they click emaillink
-const authCreateRenter = async (req, res, next) => {};
+//Endpoint for getting data on existing parking spaces by property id, owner_id
+const getPropAndSpaceInfo = async (req, res, next) => {
+  const pid = req.query.pid;
+  await spaceAndPropInfo(pid, req.user_id)
+    .then((response) => res.json(response))
+    .catch((e) =>
+      res.status(stc(e)).json({ error: e.error, message: e.message })
+    );
+};
+
+const updateSpaceInfo = async (req, res, next) => {
+  await updateSpaces(req.body)
+    .then((response) => res.json(response))
+    .catch((e) =>
+      res.status(stc(e)).json({ error: e.error, message: e.message })
+    );
+};
+
+const createNewSpaces = async (req, res, next) => {
+  try {
+    const result = await createSpaces(req.body.data);
+
+    if (result.success) {
+      res.json({ message: result.message });
+    } else {
+      res.status(500).json({ error: result.error });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 const getPropertyInfo = async (req, res, next) => {
   await getPropInfo(req.user_id)
     .then((response) => {
       res.status(200).json(response);
+    })
+    .catch((e) => {
+      res.status(stc(e)).json({ message: e.message, error: e.error });
+    });
+};
+
+const createRenterFunc = async (req, res, next) => {
+  createRenter(req.body)
+    .then((response) => {
+      res.json(response);
     })
     .catch((e) => {
       res.status(stc(e)).json({ message: e.message, error: e.error });
@@ -50,9 +82,11 @@ const renterAddressUpdate = async (req, res, next) => {
 };
 
 module.exports = {
-  createRenterFunc,
+  getPropAndSpaceInfo,
   createNewProperty,
   getPropertyInfo,
-  authCreateRenter,
+  createNewSpaces,
+  updateSpaceInfo,
+  createRenterFunc,
   renterAddressUpdate,
 };
