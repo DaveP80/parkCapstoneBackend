cd13bdf6ff1de2f1034803ebe6543d4e60a47da8
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-11-09 14:26:58 -0500

    new sql query for show page

--- routers/parkingSpotRouter.js
+++ routers/parkingSpotRouter.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
-require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -15,13 +13,36 @@ const pool = new Pool({
   },
 });
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
+  const spotId = req.params.id;
+
   try {
-    const query = "SELECT * FROM properties";
-    const result = await pool.query(query);
-    res.json({ properties: result.rows });
+    const query = `
+    SELECT p.*, s.*, 
+       cu.first_name AS client_first_name, cu.last_name AS client_last_name, cu.email AS client_email, 
+       ru.renter_id, ru.renter_address, ru.renter_email
+FROM parking_spaces p
+JOIN properties s ON p.property_lookup_id = s.property_id
+LEFT JOIN client_user cu ON p.customer_id = cu.id
+LEFT JOIN renter_user ru ON p.customer_id = ru.renter_id
+WHERE p.space_id = $1;
+
+
+    `;
+
+    console.log("SQL Query:", query);
+
+    const result = await pool.query(query, [spotId]);
+    console.log("Query Result:", result.rows);
+
+    if (result.rows.length > 0) {
+      const parkingSpot = result.rows[0];
+      res.json(parkingSpot);
+    } else {
+      res.status(404).json({ error: "Parking spot not found" });
+    }
   } catch (error) {
-    console.error("Error retrieving properties:", error);
+    console.error("Error retrieving parking spot:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
